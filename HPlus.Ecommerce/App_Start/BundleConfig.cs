978b36cb9378b984ed1a59ffb20eb5bd91b1f489
soca-git <soca.mailbox@gmail.com>
2021-05-06 19:27:38 +0100

    added missing statics

--- HPlus.Ecommerce/App_Start/BundleConfig.cs
+++ HPlus.Ecommerce/App_Start/BundleConfig.cs
@@ -23,6 +23,7 @@ namespace HPlus.Ecommerce
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/font-awesome.min.css",
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
